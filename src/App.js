4c075ab6effcedd3d548de50c6d101fe57740d6e
Seunghyeon Baek <shyun@Shyun.local>
2024-02-02 16:36:12 +0800

    seventh

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { UserProvider } from './contexts/UserContext';
 import Navigation from './navigations/Navigation';
-import UserContext from './contexts/UserContext';
-import { useState } from 'react';
-import MainStack from './navigations/MainStack';
-import { NavigationContainer } from '@react-navigation/native';
-import AuthStack from './navigations/AuthStack';
 
 const App = () => {
-  const [user, setUser] = useState(null);
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <NavigationContainer>
-        <StatusBar style="dark" />
-        {user ? <MainStack /> : <AuthStack />}
-      </NavigationContainer>
-    </UserContext.Provider>
+    <UserProvider>
+      <StatusBar style="dark" />
+      <Navigation />
+    </UserProvider>
   );
 };
 
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -7,15 +7,15 @@ import Input, {
 } from '../component/Input';
 
 import SafeInputView from '../component/SafeInputView';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Button from '../component/Button';
 import { signIn } from '../api/auth';
 import { useUserContext } from '../contexts/UserContext';
 import PropTypes from 'prop-types';
-import UserContext from '../contexts/UserContext';
 
 //상태변수 설정
 const SignInScreen = () => {
+  const { setUser } = useUserContext();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const passwordRef = useRef(null);
@@ -26,7 +26,7 @@ const SignInScreen = () => {
     setDisabled(!email || !password);
   }, [email, password]);
 
-  const onSubmit = async (setUser) => {
+  const onSubmit = async () => {
     if (!isLoading && !disabled) {
       try {
         setIsLoading(true);
@@ -44,46 +44,42 @@ const SignInScreen = () => {
   };
 
   return (
-    <UserContext.Consumer>
-      {({ setUser }) => (
-        <SafeInputView>
-          <View style={styles.container}>
-            <Image
-              source={require('../../assets/ch4_assets/main.png')}
-              style={styles.image}
-            />
-            <Input
-              title={'이메일'}
-              placeholder="your@email.com"
-              keyboardType={KeyboardTypes.EMAIL}
-              returnKeyType={ReturnKeyTypes.NEXT}
-              value={email}
-              onChangeText={(email) => setEmail(email.trim())}
-              iconName={IconNames.EMAIL}
-              onSubmitEditing={() => passwordRef.current.focus()}
-            />
-            <Input
-              ref={passwordRef}
-              title={'비밀번호'}
-              returnKeyType={ReturnKeyTypes.DONE}
-              secureTextEntry
-              value={password}
-              onChangeText={(password) => setPassword(password.trim())}
-              iconName={IconNames.PASSWORD}
-              onSubmitEditing={() => onSubmit(setUser)}
-            />
-            <View style={styles.buttonContainer}>
-              <Button
-                title="로그인"
-                onPress={() => onSubmit(setUser)}
-                disabled={disabled}
-                isLoading={isLoading}
-              />
-            </View>
-          </View>
-        </SafeInputView>
-      )}
-    </UserContext.Consumer>
+    <SafeInputView>
+      <View style={styles.container}>
+        <Image
+          source={require('../../assets/ch4_assets/main.png')}
+          style={styles.image}
+        />
+        <Input
+          title={'이메일'}
+          placeholder="your@email.com"
+          keyboardType={KeyboardTypes.EMAIL}
+          returnKeyType={ReturnKeyTypes.NEXT}
+          value={email}
+          onChangeText={(email) => setEmail(email.trim())}
+          iconName={IconNames.EMAIL}
+          onSubmitEditing={() => passwordRef.current.focus()}
+        />
+        <Input
+          ref={passwordRef}
+          title={'비밀번호'}
+          returnKeyType={ReturnKeyTypes.DONE}
+          secureTextEntry
+          value={password}
+          onChangeText={(password) => setPassword(password.trim())}
+          iconName={IconNames.PASSWORD}
+          onSubmitEditing={onSubmit}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="로그인"
+            onPress={onSubmit}
+            disabled={disabled}
+            isLoading={isLoading}
+          />
+        </View>
+      </View>
+    </SafeInputView>
   );
 };
 
